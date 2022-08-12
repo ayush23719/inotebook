6c413f650786923c7183061020356159e337d117
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-08-12 22:44:29 +0530

    router setup

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+    obj = {
+        name: 'John',
+        age: 30
+    }
+    res.json(obj)
+})
 
-
-    ')
+module.exports = router;
