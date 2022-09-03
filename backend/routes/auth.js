9467d9463e68d57bf10f976c1389793cd909abe9
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-09-03 23:17:07 +0530

    User auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/', (req, res) => {
+const User  - require('../models/User')
+// Create a User using: POST "/api/auth"/ Doesn't require auth
+router.get('/', (req, res) =>
 
     res.json([])
 })
