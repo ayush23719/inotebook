3de624baaaf8a9f993c05f116a4aab5c70fa482b
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-09-04 22:38:12 +0530

    comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
-// Create a User using: POST "/api/auth"/ Doesn't require auth
+// Create a User using: POST "/api/auth"/ No login required
 router.get('/', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
