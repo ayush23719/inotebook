07958f8991a1fd5e9cfa1dc4aa030321d03f9338
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-09-04 22:40:20 +0530

    async

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,13 +7,14 @@ router.get('/', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
-], (req, res) => {
+], async (req, res) => {
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
-    User.create({
+    // Check whether the user with this email exists already
+    let user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
