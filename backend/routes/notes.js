a201d1a9bb1d5546a637641a2bb898f6af5f7d2e
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-09-17 18:32:19 +0530

    adding notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -9,4 +9,24 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
     res.json(notes)
 })
 
+
+// ROUTE 2: Add a new note using: POST "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    // Simple validation
+    if (!title || !description || !tag) {
+        return res.status(400).json({ error: "Please fill the all fields" })
+    }
+    try {
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+        res.json(savedNote)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+    }
+})
+
 module.exports = router;
