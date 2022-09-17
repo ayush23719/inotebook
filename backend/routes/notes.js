9f010cf4ad241b934013731d530840419c0afd16
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-09-17 18:31:18 +0530

    Notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const Notes = require('../models/Notes')
+const fetchuser = require('../middleware/fetchuser')
+// ROUTE 1: Fetch all notes of a user using: GET "/api/notes/fetchallnotes". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    const notes = await Notes.find({ user: req.user.id })
 
-router.get('/', (req, res) => {
-
-    res.json([])
+    res.json(notes)
 })
 
 module.exports = router;
